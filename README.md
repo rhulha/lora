371ede854f8aa96d644b446fa348e0361adf21a7
Ray <java4life@gmail.com>
2023-05-22 17:22:07 +0200

    setup_lambdalabs.py now includes the save_pretrained patch.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # lora
-Train Large Language Models (LLM) using LoRA  
+Train Large Language Models (LLM) using Huggingface, PEFT and LoRA  
 
 I have included a script that sets up most of the things needed if you use lambdalabs.  
 It is called: setup_lambdalabs.py  
@@ -13,7 +13,7 @@ training_data_url=https://raw.githubusercontent.com/karpathy/char-rnn/master/dat
 
 The LoRA training script is called train_text.py  
 
-Please look at the header of the script to adjust settings like:  
+Please look at the header of the train_text.py script to adjust settings like:  
 
 model_name = "eachadea/vicuna-13b-1.1"  
 load_in_8bit=True  
@@ -23,3 +23,15 @@ output_dir='.'
 cutoff_len = 512  
 overlap_len = 128  
 newline_favor_len = 128  
+
+Very Important: There is currently a problem saving the LoRA model.
+User angelovAlex found a great solution here: https://github.com/rhulha/lora/issues/1
+
+The setup_lambdalabs.py will automatically apply this patch.
+
+If you don't use lambdalabs you will have to apply this patch manually.
+
+To use the LoRA model you can take a look at inference.py.
+
+It also uses hard coded values, so if you change model names you will have to adapt his script too.
+
--- setup_lambdalabs.py
+++ setup_lambdalabs.py
@@ -46,10 +46,29 @@ def setup_LD_LIBRARY_PATH():
     run_cmd("export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/")
     run_cmd('echo "export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/">>~/.bashrc')
 
-def compile_bits_and_bytes():
+def replace_in_file(file, old_string, new_string):
+        with open(file, 'r') as f:
+            s = f.read()
+            s = s.replace(old_string, new_string)
+        with open(file, 'w') as f:
+            f.write(s)
+
+def compile_bits_and_bytes(patch_save_pretrained=True):
     run_cmd("git clone https://github.com/TimDettmers/bitsandbytes")
     run_cmd("cd bitsandbytes;pip install -r requirements.txt")
     run_cmd("nvcc --version")
+
+    if patch_save_pretrained:
+        # patch_save_pretrained begin
+        # there is a problem with saving the model with running out of GPU memory
+        # this patch fixes this problem. Credit goes to angelovAlex
+        sftp = ssh.open_sftp()
+        sftp.get('/home/ubuntu/bitsandbytes/bitsandbytes/nn/modules.py', './modules.py')
+        replace_in_file('./modules.py', "self.state.CxB, self.state.tile_indices", "self.state.CxB.cpu(), self.state.tile_indices.cpu()")
+        sftp.put('./modules.py', '/home/ubuntu/bitsandbytes/bitsandbytes/nn/modules.py')
+        sftp.close()
+        # patch_save_pretrained end
+
     run_cmd("cd bitsandbytes;CUDA_VERSION=118 make cuda11x")
     run_cmd("cd bitsandbytes;sudo python setup.py install")
 
@@ -58,7 +77,8 @@ def upload_training_data():
 
 def upload_train_py():
     sftp = ssh.open_sftp()
-    sftp.put("train_text.py", '/home/ubuntu/' + "train_text.py")
+    sftp.put('train_text.py', '/home/ubuntu/train_text.py')
+    sftp.put('inference.py', '/home/ubuntu/inference.py')
     sftp.close()
 
 run_basic_setup()
